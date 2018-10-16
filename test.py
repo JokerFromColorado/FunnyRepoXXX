80551058ed0a85e1fc3fedf834333db21a0e40f6
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2018-10-16 18:44:21 +0300

    message #FRXXX-1 чddddd

--- test.py
+++ test.py
@@ -7,19 +7,4 @@ fsdfsfds
 aasdffsdsf
 12123132
 12ssss321
-asdsdaasssddas
-asddsasdasdqweqweewqйцу
-йцу
-цуй
-цуцуй
-
-we
-rre
-w
-wre
-wer
-wewrestring for rty
-rtrtyras
-xxx
-sdfgsd
-wert
+test xx
