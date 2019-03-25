dbf809ac4812f3e521f922e732a5e63e58d5936e
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-03-25 21:08:34 +0300

    message #TCI-3 Fixed Major

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ dfsdfsdf
 qweqweeqewqqew
 test test test test test test test qweqweeqwsqwe
 wqewqqweeqwqweqwe
-qweqweqweweqqweqweqwewqeqwe\\
-wqeqweqweqeweqw
+qweqweqweweqqweqweqweqweqweqwqweqwe
+wqeqweqweqeweqwqweqweqwe
