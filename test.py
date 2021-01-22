72acfcb6cf2e003239adec8da4ff9caaccee2e88
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2021-01-22 18:41:27 +0300

    message #T1-1 fixed critical task

--- test.py
+++ test.py
@@ -3,10 +3,5 @@ wqwqewqweqweqweqqw
 wqewq
 wqeeqwwqeweqweqewq
 asSaaasasddas
-wewesaasd
-asdadsadsasddsaadsqwqewqewqweqeqw
-sadsaaadssadasd
-weqeqweqweqw
-eqwwqeqweqew
-qweewqqweeqwqewqweweqwqe
-test
+asSAwq
+ewweqeqwqewqew
